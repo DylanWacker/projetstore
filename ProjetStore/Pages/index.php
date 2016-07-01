0576d7e456a0ea41292e13ed594b515fa55c71b5
DylanWacker <Dylan.wckr@eduge.ch>
2016-07-01 11:32:24 +0200

    ds
    site

---
+++ ProjetStore/Pages/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Store</title>
+   
+    <!-- Bootstrap -->
+    <link href="../CSS/menu.css" rel="stylesheet">
+    <link href="../CSS/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="../CSS/style.css" rel="stylesheet">
+    
+  </head>
+  <body>  
+      <header>
+          Header
+      </header>
+    <!-- jQuery (Necessaire pour Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Ajoute les fichiers bootstrap -->
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/MenuNavigation.js"></script>
+    <?php
+        include './MenuNavigation.php';
+    ?> 
+    
+    <section>
+    asdasdjhbnsafjkhbsadfhb
+    </section>
+    <footer>    
+     Footer
+    </footer>
+  </body>
+</html>
