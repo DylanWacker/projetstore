c8cfcfbe6983039c18e1a5f7eabeb12c708f33fa
DylanWacker <Dylan.wckr@eduge.ch>
2016-07-01 15:47:03 +0200

    button
    button

--- ProjetStore/Pages/PageLogin.php
+++ ProjetStore/Pages/PageLogin.php
@@ -16,7 +16,7 @@ if (VerifierConnection()) {
 				    <form method="post" action="connexion.php" enctype="multipart/form-data">
 					<input type="text" name="Pseudo" placeholder="Pseudo">
 					<input type="password" name="Mdp" placeholder="Mot de passe">
-					<input type="submit" name="Login" class="Login Loginmodal-submit" value="Connexion">
+					<input type="button" class="btn btn-primary btn-block" name="Login" class="Login Loginmodal-submit" value="Connexion">
 				  </form>
 					
 				  <div class="Login-help">
