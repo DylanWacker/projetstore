65acfe02fcea0b7a0508ed5f32871ec4f5e992e9
dylan555 <dylan555.dc@gmail.com>
2016-07-07 22:55:44 +0200

    h

--- ProjetStore/Pages/FormulaireProduit.php
+++ ProjetStore/Pages/FormulaireProduit.php
@@ -1,17 +1,26 @@
 <?php
-
+$Store = AfficherStoreByID($Produit['IdStore']);
 //Formulaire Produit
 echo'<a href="#" data-toggle="modal" data-target="#Produit-modal"><img src="../Images/Icone/plus.png"></a>       
 <div class="modal fade" id="Produit-modal" tabindex="-1" role="dialog" aria-labelledby="mymodalLabel" aria-hidden="true" style="display: none;">
     	  <div class="modal-dialog">
 				<div class="Produitmodal-container">
-					<h1>NOM DE PRODUIT</h1><br>
+					<h1>';echo $Store['IdStore'];echo'</h1><br>
 				    
                     <form enctype="multipart/form-data" action="Panier.php" method="post">
                         <table>
+                        
+                          
+               
 
-                             
-                        </table> 
+
+                            <a href="#">
+                                <img src="../Images/Store/Store'; echo $Store['IdStore']; echo'.jpg" width="200px" height="200px">
+                            </a><br>';
+
+                            echo $Store['NomStore'];
+                            echo'<a href="#">'; echo $Store['PrixStore']; echo'.-</a><br>';                         
+                       echo'</table> 
 
 
                        
