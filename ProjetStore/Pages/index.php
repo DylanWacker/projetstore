67ebc93a16b0dc28b4100256969e2863ca50075d
DylanWacker <Dylan.wckr@eduge.ch>
2016-07-11 23:23:55 +0200

    as
    as

--- ProjetStore/Pages/index.php
+++ ProjetStore/Pages/index.php
@@ -25,37 +25,8 @@ dbConnect();
             include './MenuNavigation.php';
             ?> 
         </nav>
-<<<<<<< HEAD
         <section>          
-=======
-        <section>
-            
-            <style type="text/css">
-            .tg  {border-collapse:collapse;border-spacing:0;border:none;}
-            .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
-            .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
-            .tg .tg-yw4l{vertical-align:top}
-            </style>              
-            <table class="tg">
-            <?php $Produits = $DB->query('SELECT * FROM store'); ?>
-            <?php foreach ($Produits as $Produit): ?>
-                <tr><th class="tg-yw4l">
-                    <div class="panel panel-success">
-                        <div class="panel-heading"> <h2><?php echo $Produit->NomStore; ?></h2></div>
-                        <div class="panel-body">
-                          <a href="#">
-                          <img src="../Images/Store/Store<?= $Produit->IdStore; ?>.jpg" width="200px" height="200px">
-                          </a></br>
-                         <a href="#"><?= $Produit->PrixStore?> .-</a><br>
-                         <?php $IdStore=$Produit->IdStore;
-                        include 'FormulaireProduit.php';?>                
-                        </div>
-                   </div>
-               </th>
->>>>>>> origin/master
-
-
-            <div class="home">
+               <div class="home">
                 <div class="row">
                     <div class="wrap">
                         <?php $Produits = $DB->query('SELECT * FROM store'); ?>
