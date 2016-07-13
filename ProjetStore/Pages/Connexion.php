7dcff175622b3fb117cb4fd36508438ef0dcb168
dylan555 <dylan555.dc@gmail.com>
2016-07-13 19:59:56 +0200

    connexion

--- ProjetStore/Pages/Connexion.php
+++ ProjetStore/Pages/Connexion.php
@@ -8,6 +8,61 @@ require '_header.php';
 include 'Mysql.php';
 include 'Fonction.php';
 dbConnect();
+$Include=FALSE;
+                if (empty($_SESSION['User']['IdClient'])) { //les membres connectes ne peuvent pas s'inscrire
+                    //il faut que toutes les variables du formulaire existent 
+                    if (isset($_POST['Pseudo']) && isset($_POST['Mdp'])) {
+                        // il faut que tous les champs soient renseignes 
+                        if ($_POST['Pseudo'] != "" && $_POST['Mdp'] != "") {
+                            dbConnect();
+
+                            //on crypte le mot de passe pour faire le test
+                            $passhache = md5($_POST['Mdp']);
+
+                            //on verifie qu'un membre a bien ce pseudo et ce mot de passe
+                            $req = $dbc->prepare('SELECT * FROM client WHERE Pseudo = :Pseudo AND MotDePasse= :Mdp ');
+                            $req->execute(array('Pseudo' => $_POST['Pseudo'], 'Mdp' => $passhache));
+                            $resultat = $req->fetch();
+
+                            //s'il n'y a pas de resultat, on renvoie a la page de connexion
+                            if (!$resultat) {
+                                $ConnexionLab =  "Identifiants incorrecte";
+                                $ConnexionError = 'Identifiants';
+                                $Include = TRUE;
+
+                            } else {
+
+
+                                /* on cree les variables de session du membre qui lui serviront pendant sa session */
+
+                                $_SESSION['User'] = $resultat;
+                                if (isset($_SESSION['User']['Pseudo'])) {
+                                    $Pseudo = $_SESSION['User']['Pseudo'];
+                                }
+                                $Info = AfficheInformation($Pseudo);
+                                $_SESSION['Profil'] = $Info;
+
+
+                                /* on renvoie sur la page d'accueil */
+                                header('Location: index.php');
+                            }
+                        } else {
+                            $ConnexionLab= "Il faut remplir tous les champs";
+                            $ConnexionError = 'Champs';
+                            $Include = TRUE;
+
+                        }
+                    } else {
+                        $ConnexionLab=  "Une erreur s'est produite";
+                        $ConnexionError = 'Error';
+                        $Include = TRUE;
+
+                    }
+                } else {
+                    $ConnexionLab=  "Connexion déjà établie";
+                    $ConnexionError = 'Connecter';
+                    
+                }
 ?>
 <script>
     function changerCouleur(elm) {
@@ -35,61 +90,13 @@ dbConnect();
             <h1>Connexion</h1>
             <article>
                 <?php
-                if (empty($_SESSION['User']['IdClient'])) { //les membres connectes ne peuvent pas s'inscrire
-                    //il faut que toutes les variables du formulaire existent 
-                    if (isset($_POST['Pseudo']) && isset($_POST['Mdp'])) {
-                        // il faut que tous les champs soient renseignes 
-                        if ($_POST['Pseudo'] != "" && $_POST['Mdp'] != "") {
-                            dbConnect();
-
-                            //on crypte le mot de passe pour faire le test
-                            $passhache = md5($_POST['Mdp']);
-
-                            //on verifie qu'un membre a bien ce pseudo et ce mot de passe
-                            $req = $dbc->prepare('SELECT * FROM client WHERE Pseudo = :Pseudo AND MotDePasse= :Mdp ');
-                            $req->execute(array('Pseudo' => $_POST['Pseudo'], 'Mdp' => $passhache));
-                            $resultat = $req->fetch();
-
-                            //s'il n'y a pas de resultat, on renvoie a la page de connexion
-                            if (!$resultat) {
-                                echo "Identifiants incorrecte";
-                                $ConnexionError = 'Identifiants';
-                                include 'FormulaireComptePHP.php';
-                            } else {
-
-
-                                /* on cree les variables de session du membre qui lui serviront pendant sa session */
-
-                                $_SESSION['User'] = $resultat;
-                                if (isset($_SESSION['User']['Pseudo'])) {
-                                    $Pseudo = $_SESSION['User']['Pseudo'];
-                                }
-                                $Info = AfficheInformation($Pseudo);
-                                $_SESSION['Profil'] = $Info;
-
-
-                                /* on renvoie sur la page d'accueil */
-                                ?>   
-                <SCRIPT LANGUAGE="JavaScript">
-                document.location.href = "index.php"
-                                </SCRIPT>
-                                <?php
-                            }
-                        } else {
-                            echo "Il faut remplir tous les champs";
-                            $ConnexionError = 'Champs';
-                            include 'FormulaireComptePHP.php';
-                        }
-                    } else {
-                        echo "Une erreur s'est produite";
-                        $ConnexionError = 'Error';
-                        include 'FormulaireComptePHP.php';
-                    }
-                } else {
-                    echo "Connexion déjà établie";
-                    $ConnexionError = 'Connecter';
+               
+                    echo $ConnexionLab;
+                    if ($Include = TRUE) {
+    include 'FormulaireComptePHP.php';
+}
                     
-                }
+                
                 ?>
             </article>
             
